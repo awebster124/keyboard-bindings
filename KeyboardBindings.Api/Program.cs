4665ccc86d41652ae2e66eab1cc8de6eb2f74bd8
awebster124 <44840355+awebster124@users.noreply.github.com>
2026-08-29 16:30:00 -0400

    feat: add EF Core SQLite persistence and seed data

--- KeyboardBindings.Api/Program.cs
+++ KeyboardBindings.Api/Program.cs
@@ -1,6 +1,20 @@
+using KeyboardBindings.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("Default")
+                       ?? "Data Source=keyboardbindings.db";
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(connectionString));
+
 var app = builder.Build();
+
+// Apply pending migrations on startup (they also carry the seed data) so the app is runnable out of the box.
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.MapGet("/", () => "KeyboardBindings API");
 
